b3881c0c578456dfc1abdbea5897422d15abd014
aljaz-kokol <52831109+aljaz-kokol@users.noreply.github.com>
2021-08-30 17:02:32 +0200

    BACKEND: Added isAdmin middleware

---
+++ backend-node/src/middleware/is-admin.middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors/custom.error';
+import User from '../model/user.model';
+
+export const isAdmin = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const user = await User.findById(req.userId);
+        if (!user?.isAdmin) {
+            const error = new CustomError('User does not have admin privileges');
+            error.statusCode = 401;
+            error.name = 'Not Authorized';
+            throw error;
+        }
+        next();
+    } catch(err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+   
+}
--- backend-node/src/routes/car-class.routes.ts
+++ backend-node/src/routes/car-class.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { getCarClasses, getCarClassFromId, createCarClass } from '../controller/car-class.controller';
+import { isAdmin } from '../middleware/is-admin.middleware';
+import { isAuth } from '../middleware/is-auth.middleware';
 
 const router = Router();
 
 router.get('/', getCarClasses);
 router.get('/:classId', getCarClassFromId);
 
-router.post('/', createCarClass);
+router.post('/', [
+    isAuth,
+    isAdmin
+], createCarClass);
 
 export default router;
